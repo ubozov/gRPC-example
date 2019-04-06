9e63a257f2f09c96d9f9aa0170df4c495b6288c1
Urmat Bozov <urmat@privatix.io>
2019-04-06 13:28:34 +0600

    graceful shutdown

--- main.go
+++ main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"context"
 	"log"
 	"os"
+	"os/signal"
+	"syscall"
 
 	"github.com/ubozov/grpc-example/cli"
 )
@@ -10,6 +13,16 @@ import (
 func main() {
 	log.Println("start program")
 	defer log.Println("end program")
+
+	var gracefulStop = make(chan os.Signal)
+	signal.Notify(gracefulStop, os.Interrupt, syscall.SIGTERM, syscall.SIGINT)
+	go func() {
+		sig := <-gracefulStop
+		log.Printf("\ncaught sig: %+v", sig)
+		log.Println("Wait for 2 second to finish processing")
+		cli.Shutdown(context.Background())
+		os.Exit(0)
+	}()
 
 	cli.Execute(os.Args[1:])
 }
